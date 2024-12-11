828c31f942e8913ab2af53a2841c180586c5b7e1
Khue Doan <mail@khuedoan.com>
2024-12-11 23:57:39 +0700

    refactor: rewrite with only std

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/ServiceWeaver/weaver"
 )
 
 const (
@@ -17,37 +14,38 @@ const (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Query().Get("name"))
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello, %s!\n", name)
 }
 
 func info(w http.ResponseWriter, r *http.Request) {
-	hostname, _ := os.Hostname()
+	hostname, err := os.Hostname()
+	if err != nil {
+		http.Error(w, "Failed to get hostname", http.StatusInternalServerError)
+		return
+	}
 	fmt.Fprintf(w, "App version: %s\n", appVersion)
 	fmt.Fprintf(w, "Hostname: %s\n", hostname)
 	fmt.Fprintf(w, "Time: %s\n", time.Now().Local())
 }
 
 func main() {
-	port := defaultPort
-
-	if runtimePort := os.Getenv("PORT"); runtimePort != "" {
-		port = runtimePort
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
 	}
-
-	root := weaver.Init(context.Background())
-
-	opts := weaver.ListenerOptions{
-		LocalAddress: fmt.Sprintf(":%s", port),
-	}
-
-	lis, err := root.Listener("example", opts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("example listener available on %v\n", lis)
 
 	http.HandleFunc("/", info)
 	http.HandleFunc("/info", info)
 	http.HandleFunc("/hello", hello)
-	http.Serve(lis, nil)
+
+	addr := fmt.Sprintf(":%s", port)
+	fmt.Printf("Server running on %s\n", addr)
+
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
 }
